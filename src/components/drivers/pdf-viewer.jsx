684b1bdb40180e62d3bb7390c28aab667ae4d009
John Fleming <jfleming@forj.ai>
2026-04-02 14:50:26 -0400

    Revert "Switch to native pdf display instead of react-pdf"
    This reverts commit fb2e541cbdab10d5ec8913249b4fac5ee25b3464.

--- src/components/drivers/pdf-viewer.jsx
+++ src/components/drivers/pdf-viewer.jsx
@@ -1,15 +1,46 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import pdfjsWorkerUrl from 'pdfjs-dist/build/pdf.worker.min.mjs';
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorkerUrl;
 
 function PDFDriver(props) {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
   return (
-    <object
-      data={props.filePath}
-      type="application/pdf"
-    >
-      <p>Unable to display PDF. <a href={props.filePath}>Download instead.</a></p>
-    </object>
+    <div>
+      <Document
+        file={props.filePath}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <button
+          disabled={pageNumber <= 1}
+          onClick={() => setPageNumber(p => p - 1)}
+        >
+          Previous
+        </button>
+        <span>Page {pageNumber} of {numPages}</span>
+        <button
+          disabled={pageNumber >= numPages}
+          onClick={() => setPageNumber(p => p + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
 
